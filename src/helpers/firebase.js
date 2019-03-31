73ca15e2065cb4b5f4453e07c6a39ec2e1b2e0c0
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-03-31 18:51:26 +0200

    added fileupload

---
+++ src/helpers/firebase.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyCbx3I4nYiP7Xr6RF9m1VgWrgHQcYI__ps",
+  authDomain: "ironhack-guide-me.firebaseapp.com",
+  databaseURL: "https://ironhack-guide-me.firebaseio.com",
+  projectId: "ironhack-guide-me",
+  storageBucket: "ironhack-guide-me.appspot.com",
+  messagingSenderId: "532856559222"
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { config } from './helpers/firebase';
+import firebase from 'firebase'
+
+firebase.initializeApp(config)
 
 ReactDOM.render(
   <Router>
